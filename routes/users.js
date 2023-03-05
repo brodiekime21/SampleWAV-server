b8e96e5ee68a684232507e5c4a3e16c11009487e
Brodie Kime <brodie.kime@gmail.com>
2023-03-05 08:59:02 -0500

    working on editing profile and axios.put

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,17 @@
+
 var express = require('express');
 var router = express.Router();
 
-const User = require('../models/User')
+const User = require('../models/User');
 
 /* GET users listing. */
 router.get('/profile/:userId', (req, res, next) => {
   User.findById(req.params.userId)
-    .populate('users')
     .populate('samples')
     .populate('packs')
+    .populate('reposts')
+    .populate('followers')
+    .populate('following')
     .then((foundUser) => {
       res.json(foundUser)
     })
@@ -17,16 +20,15 @@ router.get('/profile/:userId', (req, res, next) => {
     })
 });
 
-router.post('/profile-edit/:userId', (req, res, next) => {
+router.put('/edit-profile/:userId', (req, res, next) => {
   User.findByIdAndUpdate(req.params.userId, 
     {
       artist_name: req.body.artist_name,
-      profile_image: req.body.profile_image,
-      location: req.body.location,
-      age: req.body.age,
-      bio: req.body.bio,
-      social_links: req.body.social_links
-      
+      // profile_image: req.body.profile_image,
+      // location: req.body.location,
+      // age: req.body.age,
+      // bio: req.body.bio,
+      // social_links: req.body.social_links
     },
     {new: true}
     )
